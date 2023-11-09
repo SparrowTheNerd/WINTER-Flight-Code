d9be8672524aef8a301873f569df5911bf5183eb
Sparrowhawk120 <theworldofbirdsmaker@gmail.com>
2023-11-08 20:10:36 -0500

    starting from the (kinda) beginning

--- src/Kalman/KalmanFilter.h
+++ src/Kalman/KalmanFilter.h
@@ -11,40 +11,10 @@ class KalmanFilter {
 			float w;          //real value
 			float x, y, z;    //imaginary values
 		};
-		KalmanFilter(Sensors &sens);
-		void init(float stddev_mg, float stddev_ps);
-		void filter(float dt);
-		Matrix<10> x; //x_n,n
+		
 		
 
 	private:
-		Matrix<4> uXl; // cntrl vector for accel
-		Matrix<3> uGy; // cntrl vector for gyros
-		Matrix<4> zMg; // obs vector for magns
-		Matrix<1> zBm; // obs vector for press
-		Matrix<10> x_prior; //x_n,n-1
-		Matrix<4,10> Hmg;  //mag obs matrix
-		Matrix<1,10> Hbm;  //baro obs matrix
-		Matrix<10,4> G;    //cntrl matrix
-		Matrix<10,10> F;   //state transn matrix
-		Matrix<10,10> P;   //covariance matrix
-		Matrix<10,10> P_prior;  //covariance prior prediction
-		Matrix<4,4> Rmg;   //mag meas covar matrix
-		Matrix<1,1> Rbm;   //baro meas covar matrix
-		Matrix<10,4> Kmg;  //mag kalman gain
-		Matrix<10>   Kbm;  //baro kalman gain
-
-		LSM9DS1 IMU;
-		Matrix<3> mgBase;  //initial magnetometer vector, for comparing against
-
-		Sensors& sens;
-
-		void gain();
-		void update(bool magAvail, bool baroAvail);
-		void extrapolate(float dt);
-		void normalize();
-		Matrix<3> crossProd(Matrix<3> A, Matrix<3> B);
-		Matrix<3> measTransform(quaternion q, Matrix<3> meas);
-		int normCounter=0;
+		
 
 };
--- src/main.cpp
+++ src/main.cpp
@@ -3,88 +3,15 @@
 #include "Kalman/KalmanFilter.h"
 #include "Sensors/Sensors.h"
 
-#define BZR     PC8
-#define CS_SD   PC0
-#define gXOfst 1.301940492
-#define gYOfst 0.859327296
-#define gZOfst 0.528033635
-//measurement stddevs
-#define stddev_xl   (float)0.180   //180 milli G's from LSM9DS1 datasheet
-#define stddev_gy   (float)2.0     //2 deg/s, roughly measured
-#define stddev_mg   (float)0.1     //1 Gauss from LSM9DS1 datasheet, will improve post calibration
-#define stddev_ps   (float)3.05     //400Pa, around 10ft (3.05m) from MS5607 datasheet
-
-SdFat SD;
-File file;
-
-uint32_t Time;
-bool magAvail;  //is there mag data available?
-bool baroAvail = false; //is there baro data available?
-char fileName[] = "FLIGHTDATA00.bin";
-float dT;
-struct sensData {   
-  float time, gX, gY, gZ, aX, aY, aZ, mX, mY, mZ, prs, tmp;
-} data;
-
-
-Matrix<3,1> magCal_hard = {-19.03*8,28.14*8,-66.63*8}; //hard and soft iron calibrations
-Matrix<3,3> magCal_soft = { 0.961, 0.026,-0.024,
-                            0.026, 1.010,-0.002,
-                           -0.024,-0.002, 1.032};
-Matrix<3> mag;
-
-float degToRad(float deg) {
-  return deg*DEG_TO_RAD;
-}
-float radToDeg(float rad) {
-  return rad*RAD_TO_DEG;
-}
-
-Sensors sens = Sensors(magCal_hard,magCal_soft, gXOfst, gYOfst, gZOfst);
-KalmanFilter kalman = KalmanFilter(sens);
-
 void setup() {
-  pinMode(BZR,OUTPUT);
-  analogWriteResolution(16);  //set pwm resolution to 16 bit (0-65535)
-  analogWriteFrequency(1000); //set pwm frequency to 1KHz
-
   SerialUSB.begin(); //start serial port
   while(!SerialUSB);
 
   Wire.begin(uint32_t(PB9_ALT0),uint32_t(PB8_ALT0));
-
-  sens.init();    //initialize sensors
-  sens.timeBaro = micros();
-  kalman.init(stddev_mg, stddev_ps);
-  Serial << kalman.x;
-  Time = micros();
-  delay(1);
 }
 
-int counter = 0;
 void loop() {
-  dT = (float)(micros()-Time)/1000000.;
-  Time = micros();
-  sens.getData();
-  kalman.filter(dT);
-  sens.magAvail = false; sens.baroAvail = false;  //reset sensor availability before next loop
-  // Serial << kalman.x;
-  // Serial.println(" ");
-}
-
-// void writeToSD() {
-//   file.write((uint8_t *)&data,sizeof(data)/sizeof(uint8_t));
-// }
-
-void sdInit() {     //initialize SD card and filename
-  SD.begin(CS_SD, SPI_FULL_SPEED);
-  for (uint8_t i = 0; i < 100; i++) {
-    fileName[10] = i/10 + '0';
-    fileName[11] = i%10 + '0';
-    if (SD.exists(fileName)) continue;
-    file.open(fileName);
-    break;
-  }
+  
 }
 
 void SystemClock_Config(void)
